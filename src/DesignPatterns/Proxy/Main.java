970ed2bd52ad6a966cd210d38d979a13af07c782
Daniel <danielcorreia1988@outlook.com>
2023-01-22 11:28:35 +0100

    Created proxy pattern.

---
+++ src/DesignPatterns/Proxy/Main.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Proxy;
+
+public class Main {
+
+    /**
+     * GoF Definition:
+     * 
+     * Provide a surrogate or placeholder for another object to control access to
+     * it.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO: Do this
+    }
+}
