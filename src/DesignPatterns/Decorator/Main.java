97baf1873cb2b1b6c510ba0607de53db50e148ad
Daniel <danielcorreia1988@outlook.com>
2023-01-22 17:24:10 +0100

    Created Decorator pattern.

---
+++ src/DesignPatterns/Decorator/Main.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Decorator;
+
+public class Main {
+
+    /**
+     * GoF Definition:
+     * 
+     * Attach additional responsabilities to an object dynamically. Decorators provide a
+     * flexible alternative to subclassing for extending functionality.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO: Do Decorator pattern here.
+    }
+}
