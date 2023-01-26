6487fa9c0591f6431aa3a813b7c6f5717a4c3fc4
Daniel <danielcorreia1988@outlook.com>
2023-01-26 11:00:14 +0100

    Created Adtapter pattern.

---
+++ src/DesignPatterns/Adapter/Main.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Adapter;
+
+public class Main {
+
+    /**
+     * GoF Definition:
+     * 
+     * Convert the interface of a class into another interface that clients expect. Adapter lets
+     * classes work together that could not otherwise because of incompatible interfaces.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO:
+    }
+}
